8fb8dfcf34e47af3d600a0f76a2c5f3948b84bb3
Muh Ihsan Nur <canks.official@gmail.com>
2025-09-23 20:01:51 +0800

    Add autoexec.lua for player kick and rejoinAuto exec

---
+++ autoexec.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+wait(3) -- tunggu IY ke-load
+game:service("Players").LocalPlayer:Kick(";wp summit")
+wait(5) -- delay 5 detik sebelum rejoin
+game:service("Players").LocalPlayer:Kick(";rj")
